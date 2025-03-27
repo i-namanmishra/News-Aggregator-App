4d9a6047a8ba927266f4dc43c7c14aedec8853b2
itsNamanmishra <naman.mishra_cs22@gla.ac.in>
2025-03-27 11:00:42 +0530

    #

--- News-Aggregator/src/App.jsx
+++ News-Aggregator/src/App.jsx
@@ -1,16 +1,9 @@
 
 import React from 'react'
-import {NewsHeader} from './components/JSX/NewsHeader'
-import { NewsFooter } from './components/JSX/NewsFooter'
-import { NewsHome } from './components/JSX/NewsHome'
 import './App.css'
 const App = () => {
   return (
-    <div>
-      <NewsHeader/>
-      <NewsHome/>
-      <NewsFooter/>
-    </div>
+    <></>
   )
 }
 
